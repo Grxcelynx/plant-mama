70f7fd13830a73c2f5ad2aa13334a708ef9537ed
Grace Birnam <grace.mbirnam@gmail.com>
2022-07-01 21:14:44 -0700

    inital

---
+++ front-page.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Template Name: Homepage
+*
+* @package Plant_Mama
+*
+*/
+?>
+<?php get_header(); ?>
+
+<div>
+    <h1>Main page</h1>
+    <h2>subtitle for main page</h2>
+</div>
+
+<div>
+    
+</div>
